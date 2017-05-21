ba4d6669741edd4390f3a18a467444d1a4d1230d
Denis Papathanasiou <denis@papathanasiou.org>
2017-05-21 13:27:34 -0400

    Meh, 'foo <> null' is still a thing

--- src/i_introduction/_7_Nullable_Types/NullableTypes.kt
+++ src/i_introduction/_7_Nullable_Types/NullableTypes.kt
@@ -1,8 +1,5 @@
 package i_introduction._7_Nullable_Types
 
-import util.TODO
-import util.doc7
-
 fun test() {
     val s: String = "this variable cannot store null references"
     val q: String? = null
@@ -12,20 +9,11 @@ fun test() {
     val j: Int = q?.length ?: 0  // 0
 }
 
-fun todoTask7(client: Client?, message: String?, mailer: Mailer): Nothing = TODO(
-    """
-        Task 7.
-        Rewrite JavaCode7.sendMessageToClient in Kotlin, using only one 'if' expression.
-        Declarations of Client, PersonalInfo and Mailer are given below.
-    """,
-    documentation = doc7(),
-    references = { JavaCode7().sendMessageToClient(client, message, mailer) }
-)
-
-fun sendMessageToClient(
-        client: Client?, message: String?, mailer: Mailer
-) {
-    todoTask7(client, message, mailer)
+fun sendMessageToClient(client: Client?, message: String?, mailer: Mailer) {
+    val email = client?.personalInfo?.email ?: return
+    if( null != message ) {
+        mailer.sendMessage(email, message)
+    }
 }
 
 class Client (val personalInfo: PersonalInfo?)
