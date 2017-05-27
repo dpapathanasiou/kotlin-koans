b927f2b0e77167608f9d8a3a58050efafe7ae509
Denis Papathanasiou <denis@papathanasiou.org>
2017-05-27 19:34:52 -0400

    Map builder exercise

--- src/v_builders/_37_StringAndMapBuilders.kt
+++ src/v_builders/_37_StringAndMapBuilders.kt
@@ -1,6 +1,5 @@
 package v_builders
 
-import util.TODO
 import java.util.*
 
 fun buildStringExample(): String {
@@ -19,21 +18,17 @@ fun buildStringExample(): String {
     }
 }
 
-fun todoTask37(): Nothing = TODO(
-    """
-        Task 37.
-        Uncomment the commented code and make it compile.
-        Add and implement function 'buildMap' with one parameter (of type extension function) creating a new HashMap,
-        building it and returning it as a result.
-    """
-)
+fun <K, V> buildMap(build: HashMap<K, V>.() -> Unit): Map<K, V> {
+    val map = HashMap<K, V>()
+    map.build()
+    return map
+}
 
 fun task37(): Map<Int, String> {
-    todoTask37()
-//    return buildMap {
-//        put(0, "0")
-//        for (i in 1..10) {
-//            put(i, "$i")
-//        }
-//    }
+    return buildMap {
+        put(0, "0")
+        for (i in 1..10) {
+            put(i, "$i")
+        }
+    }
 }
