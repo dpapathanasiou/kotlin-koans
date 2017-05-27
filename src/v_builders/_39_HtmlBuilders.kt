ed2b5234bdf1815aef3d671412cfc6b0b2e5c5e1
Denis Papathanasiou <denis@papathanasiou.org>
2017-05-27 19:43:01 -0400

    HTML generation with builders

--- src/v_builders/_39_HtmlBuilders.kt
+++ src/v_builders/_39_HtmlBuilders.kt
@@ -1,23 +1,10 @@
 package v_builders
 
-import util.TODO
-import util.doc39
 import v_builders.data.getProducts
 import v_builders.htmlLibrary.*
 
 fun getTitleColor() = "#b9c9fe"
 fun getCellColor(row: Int, column: Int) = if ((row + column) %2 == 0) "#dce4ff" else "#eff2ff"
-
-fun todoTask39(): Nothing = TODO(
-    """
-        Task 39.
-        1) Fill the table with the proper values from products.
-        2) Color the table like a chess board (using getTitleColor() and getCellColor() functions above).
-        Pass a color as an argument to functions 'tr', 'td'.
-        You can run the 'Html Demo' configuration in IntelliJ IDEA to see the rendered table.
-    """,
-    documentation = doc39()
-)
 
 fun renderProductTable(): String {
     return html {
@@ -34,7 +21,19 @@ fun renderProductTable(): String {
                 }
             }
             val products = getProducts()
-            todoTask39()
+            for((ind, prod) in products.withIndex()) {
+                tr {
+                    td(color = getCellColor(ind, 0)) {
+                        text(prod.description)
+                    }
+                    td(color = getCellColor(ind, 1)) {
+                        text(prod.price)
+                    }
+                    td(color = getCellColor(ind, 2)) {
+                        text(prod.popularity)
+                    }
+                }
+            }
         }
     }.toString()
 }
