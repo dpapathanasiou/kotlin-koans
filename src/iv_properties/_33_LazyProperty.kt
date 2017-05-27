87e485b8ffdcae9a3f1b7bbe4f63191c912a2fda
Denis Papathanasiou <denis@papathanasiou.org>
2017-05-27 18:40:13 -0400

    Lazy properties: more null un-goodness

--- src/iv_properties/_33_LazyProperty.kt
+++ src/iv_properties/_33_LazyProperty.kt
@@ -1,19 +1,12 @@
 package iv_properties
 
-import util.TODO
-
 class LazyProperty(val initializer: () -> Int) {
-    val lazy: Int = todoTask33()
+    var value: Int? = null
+    val lazy: Int
+        get() {
+            if (value == null) {
+                value = initializer()
+            }
+            return value!!
+        }
 }
-
-fun todoTask33(): Nothing = TODO(
-    """
-        Task 33.
-        Add a custom getter to make the 'lazy' val really lazy.
-        It should be initialized by the invocation of 'initializer()'
-        at the moment of the first access.
-        You can add as many additional properties as you need.
-        Do not use delegated properties!
-    """,
-    references = { LazyProperty({ 42 }).lazy }
-)
